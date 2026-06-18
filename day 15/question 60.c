468df93654cac0e182e7670e6bc1604659801487
ankityadav07112005-art <ankityadav07112005@gmail.com>
2026-06-18 16:17:46 +0530

    Add files via upload

---
+++ day 15/question 60.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[100];
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    int count = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] != 0) {
+            arr[count] = arr[i];
+            count++;
+        }
+    }
+    while(count < n) {
+        arr[count] = 0;
+        count++;
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
