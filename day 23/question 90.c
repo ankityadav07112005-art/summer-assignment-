8b5a35e0715a242d889e6aa181fc08413d32784f
ankityadav07112005-art <ankityadav07112005@gmail.com>
2026-06-28 16:29:56 +0530

    Add files via upload

---
+++ day 23/question 90.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(freq[(unsigned char)str[i]] == 1) {
+            printf("First repeating character is: %c\n", str[i]);
+            return 0;
+        }
+        freq[(unsigned char)str[i]] = 1;
+    }
+
+    printf("No repeating character found.\n");
+    return 0;
+}
